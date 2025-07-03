39bc105c80c028a58c9204543c87903cd2c3da6f
Parth Saini <parth24saini87@gmail.com>
2025-07-03 15:12:38 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,34 @@
+// #include<iostream> 
+// using namespace std;
+
+// int main()
+// {
+//     cout <<"Hellow\nWorld";
+//     return 0;
+// }
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,count=0;
+    cout<< "Enter a number:";
+    cin>>n;
+    for(int i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            count++;
+        }
+    }
+    if(count==0)
+    {
+        cout<<"Prime Number";
+    }
+    else
+    {
+        cout<<"Not a prime number";
+    }
+    return 0;
+}
